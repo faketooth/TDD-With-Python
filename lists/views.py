f4d98d573c652ee1e77a836616a08e964c2f98ed
l00227316 <liuxiaojie.liu@hauwei.com>
2015-12-15 13:50:12 +0800

    chapter 5.4 just before refactor.

--- lists/views.py
+++ lists/views.py
@@ -3,7 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home_page(request):
-	print(request.method)
 	return render(request, 'home.html', {
 		'new_item_text': request.POST.get('item_text', ''),
 	})
