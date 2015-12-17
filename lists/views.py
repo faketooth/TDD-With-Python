f0bc6728e1f8a2fa8a40c1586268f27f8db73dd0
l00227316 <liuxiaojie.liu@hauwei.com>
2015-12-17 11:47:31 +0800

    redirect after saving item.

--- lists/views.py
+++ lists/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from lists.models import Item
 
 # Create your views here.
 def home_page(request):
-	return render(request, 'home.html', {
-		'new_item_text': request.POST.get('item_text', ''),
-	})
+	if request.method == 'POST':
+		#new_item_text = request.POST['item_text']
+		#Item.objects.create(text=new_item_text)
+		Item.objects.create(text=request.POST['item_text'])
+		return redirect('/')
+	#else:
+	#	new_item_text = ''
+	#item = Item()
+	#item.text = request.POST.get('item_text', '')
+	#item.save()
+	
+	#return render(request, 'home.html', {
+	#	'new_item_text': new_item_text, #request.POST.get('item_text', ''),
+	#})
+	return render(request, 'home.html')
