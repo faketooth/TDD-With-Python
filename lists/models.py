d423d5659334e403dc6ea97cda964b14b16f8f9e
l00227316 <liuxiaojie.liu@hauwei.com>
2016-01-09 17:03:43 +0800

    Changes to be committed: 	new file:   lists/migrations/0003_list.py 	new file:   lists/migrations/0004_item_list.py 	new file:   lists/migrations/0005_auto_20160109_1632.py 	new file:   lists/migrations/0006_auto_20160109_1638.py 	new file:   lists/migrations/0007_auto_20160109_1643.py 	modified:   lists/models.py 	modified:   lists/tests.py 	modified:   lists/views.py

--- lists/models.py
+++ lists/models.py
@@ -2,5 +2,14 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class List(models.Model):
+	pass
+
+	
+		
 class Item(models.Model):
 	text = models.TextField(default='')
+	#django.db.utils.IntegrityError: NOT NULL constraint failed: lists_item.list_id
+	#list = models.ForeignKey(List, null=True, default=None)
+	list = models.ForeignKey(List, default=None)
+
